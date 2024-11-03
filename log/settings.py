e4b93a02f112820acbbe71e716beabfa99768333
Bobby Goel <easyspi@gmail.com>
2024-11-03 06:14:58 -0500

    updates for static

--- log/settings.py
+++ log/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
