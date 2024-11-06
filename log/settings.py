99260e226ce9f9afd9ef392b97f2c91dbf7acad2
Bobby Goel <easyspi@gmail.com>
2024-11-05 19:11:42 -0500

    changes for vercel

--- log/settings.py
+++ log/settings.py
@@ -14,10 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.vercel.app','.now.sh']
 
 # Application definition
 INSTALLED_APPS = [
