a2a4ce808127787a40311f752647c37d24680499
Bobby Goel <easyspi@gmail.com>
2024-11-12 22:03:05 -0500

    log/urls.py

--- log/urls.py
+++ log/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('supermax/', admin.site.urls),
     path('', include('core.urls')),  # Include your core app's URLs
     path('4s/', include('risk.urls')),  # Include your corerisk app's URLs
-    path('supermax/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
